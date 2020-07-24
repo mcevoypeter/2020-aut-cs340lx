b2121b5a3b495c27530152674f057a4aba98b823
Wes McEvoy <wesmcevoy@MacBook-Pro.lan>
2020-07-23 20:18:30 -0600

    [completed] 20-user-process part1

--- pix/pix.c
+++ pix/pix.c
@@ -1,18 +1,20 @@
 #include "pix.h"
 #include "rpi.h"
 
-#if 0
 // you'll probably need some of this for mmu
+#include "init-hack.h"
 #include "rpi-interrupts.h"
 #include "rpi-constants.h"
 #include "mmu.h"
 #include "cpsr-util.h"
-#endif
 
-#include "init-hack.h"
+enum { OneMB = 1 << 20 };
+enum { dom1 = 1 };
 
 // gross: for the moment we just allow system calls of max 3 arguments (will change)
 typedef long (*syscall_t)();
+
+static fld_t *pt;
 
 static syscall_t syscalls[SYS_LAST] = {
     [SYS_EXIT] = (syscall_t)sys_exit,
@@ -20,6 +22,10 @@ static syscall_t syscalls[SYS_LAST] = {
 };
 
 static int pix_cur_pid = 0;
+
+int syscall_vector(unsigned pc, uint32_t r0, uint32_t r1, uint32_t r2) {
+    unimplemented();
+}
 
 void sys_exit(int code) {
     printk("%d: exiting with code <%d>\n", pix_cur_pid, code);
@@ -44,11 +50,63 @@ void no_return(void) {
     panic("impossible: returned!\n");
 }
 
+static void map_addr_space(uint32_t user_code, int32_t dom_id) {
+    // 1. init
+    mmu_init();
+    assert(!mmu_is_enabled());
+
+    void *base = (void*)0x100000;
+
+    pt = mmu_pt_alloc_at(base, 4096*4);
+    assert(pt == base);
+
+    // 2. setup mappings
+
+    // map the first MB: shouldn't need more memory than this.
+    mmu_map_section(pt, 0x0, 0x0, dom_id);
+    // map the page table: for lab cksums must be at 0x100000.
+    mmu_map_section(pt, 0x100000,  0x100000, dom_id);
+    // map stack (grows down)
+    mmu_map_section(pt, STACK_ADDR-OneMB, STACK_ADDR-OneMB, dom_id);
+    // map user stack (grows down)
+    mmu_map_section(pt, STACK_ADDR2-OneMB, STACK_ADDR2-OneMB, dom_id);
+    // map user code
+    mmu_map_section(pt, user_code, user_code, dom_id);
+
+    // map the GPIO: make sure these are not cached and not writeback.
+    // [how to check this in general?]
+    mmu_map_section(pt, 0x20000000, 0x20000000, dom_id);
+    mmu_map_section(pt, 0x20100000, 0x20100000, dom_id);
+    mmu_map_section(pt, 0x20200000, 0x20200000, dom_id);
+
+    // if we don't setup the interrupt stack = super bad infinite loop
+    mmu_map_section(pt, INT_STACK_ADDR-OneMB, INT_STACK_ADDR-OneMB, dom_id);
+
+    // 3. install fault handler to catch if we make mistake.
+    mmu_install_handlers();
+
+    // 4. start the context switch:
+
+    // set up permissions for the different domains: we only use <dom_id>
+    // and permissions r/w.
+    write_domain_access_ctrl(0b01 << dom_id*2);
+
+    // use the sequence on B2-25
+    set_procid_ttbr0(0x140e, dom_id, pt);
+
+    // turn on mmu
+    mmu_enable();
+    assert(mmu_is_enabled());
+}
 
 static void run(unsigned *code, unsigned nbytes) {
-    uint32_t *dest_addr = (uint32_t *)code[0];
-    memcpy(dest_addr, code, nbytes);
-    BRANCHTO((uint32_t)(dest_addr+1));
+    // copy code to target addr
+    uint32_t dest_addr = code[0];
+    memcpy((void *)dest_addr, code, nbytes);
+
+    map_addr_space(dest_addr, dom1);
+
+    BRANCHTO(dest_addr + sizeof(uint32_t));
     no_return();
 }
 
